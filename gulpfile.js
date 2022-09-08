217540440d73ff1964fa9f6088abfa4dc119ccbf
maleja274 <maleja274@hotmail.com>
2022-09-07 22:54:01 -0500

    gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-const {src, dest } = require("gulp");
-const sass = require("sass");
+const {src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require('sass'));
 
 
 function css(done) {
@@ -16,5 +16,13 @@ function css(done) {
     done();// callback que avisa a gulp cuando llegamos al final 
 }
 
+function dev(done) {
+    watch("src/scss/app.scss", css)
+
+
+    done();
+}
+
 exports.css = css;
+exports.dev = dev;
 
